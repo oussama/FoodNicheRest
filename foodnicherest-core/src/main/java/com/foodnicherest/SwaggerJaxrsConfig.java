57c47d9d51a195a1b1023a979dfadae8d742962a
Obianuju Ezeoke <ujuezeoke@gmail.com>
2015-11-24 20:02:00 +0000

    Swagger base url is not hard coded

--- foodnicherest-core/src/main/java/com/foodnicherest/SwaggerJaxrsConfig.java
+++ foodnicherest-core/src/main/java/com/foodnicherest/SwaggerJaxrsConfig.java
@@ -26,7 +26,7 @@ public class SwaggerJaxrsConfig extends HttpServlet {
             super.init(servletConfig);
             SwaggerConfig swaggerConfig = new SwaggerConfig();
             ConfigFactory.setConfig(swaggerConfig);
-            swaggerConfig.setBasePath("http://localhost:31831/FoodNicheRest/rest");
+            swaggerConfig.setBasePath("http://localhost:8080/FoodNicheRest/rest");
             swaggerConfig.setApiVersion("1.0.0");
             ScannerFactory.setScanner(new DefaultJaxrsScanner());
             ClassReaders.setReader(new DefaultJaxrsApiReader());
