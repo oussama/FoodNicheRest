d13f4f56e454213aaae921e711dffb2fd2a519da
Obianuju Ezeoke <ujuezeoke@gmail.com>
2015-12-05 22:35:14 +0000

    The beginings of dev specific properties. And Profile that updates database

---
+++ foodniche-db/src/main/sql/2.dml.sql
@@ -0,0 +1,33 @@
+
+  INSERT INTO SubcsriptionTypes(SubscriptionTypeID, Name) VALUES (0, 'Business Basic');
+  INSERT INTO SubcsriptionTypes(SubscriptionTypeID, Name) VALUES (1, 'Business Siver');
+  INSERT INTO SubcsriptionTypes(SubscriptionTypeID, Name) VALUES (2, 'Business Gold');
+  INSERT INTO SubcsriptionTypes(SubscriptionTypeID, Name) VALUES (3, 'Business Premium');
+  INSERT INTO SubcsriptionTypes(SubscriptionTypeID, Name) VALUES (4, 'Indivudual Basic');
+  INSERT INTO SubcsriptionTypes(SubscriptionTypeID, Name) VALUES (5, 'Individual Premium');
+
+  INSERT INTO BusinessTypes(BusinessTypeID, Name) VALUES (0, 'Fast casual restaurant');
+  INSERT INTO BusinessTypes(BusinessTypeID, Name) VALUES (1, 'Fine dining restaurant');
+  INSERT INTO BusinessTypes(BusinessTypeID, Name) VALUES (2, 'Cafe-pub restaurant');
+  INSERT INTO BusinessTypes(BusinessTypeID, Name) VALUES (3, 'Deli restaurant');
+  INSERT INTO BusinessTypes(BusinessTypeID, Name) VALUES (4, 'Food producer');
+  INSERT INTO BusinessTypes(BusinessTypeID, Name) VALUES (5, 'Bakeries restaurants');
+
+  INSERT INTO CouponTypes(CouponTypeID, Description) VALUES (0, 'Entrance');
+  INSERT INTO CouponTypes(CouponTypeID, Description) VALUES (1, 'Drinks');
+  INSERT INTO CouponTypes(CouponTypeID, Description) VALUES (2, 'Meal');
+
+  INSERT INTO ContentTypes(ContentTypeID, Name) VALUES (0, 'Review');
+  INSERT INTO ContentTypes(ContentTypeID, Name) VALUES (1, 'Photo');
+  INSERT INTO ContentTypes(ContentTypeID, Name) VALUES (2, 'Video');
+  INSERT INTO ContentTypes(ContentTypeID, Name) VALUES (3, 'Tuturial');
+  INSERT INTO ContentTypes(ContentTypeID, Name) VALUES (4, 'Menu Items');
+
+  INSERT INTO FoodPreferences(FoodPreferenceID, Name) VALUES (0, 'Vegetarian');
+  INSERT INTO FoodPreferences(FoodPreferenceID, Name) VALUES (1, 'Kosher');
+  INSERT INTO FoodPreferences(FoodPreferenceID, Name) VALUES (2, 'Gluten-free');
+  INSERT INTO FoodPreferences(FoodPreferenceID, Name) VALUES (3, 'Ethic cuisine');
+  INSERT INTO FoodPreferences(FoodPreferenceID, Name) VALUES (4, 'American cuisine');
+  INSERT INTO FoodPreferences(FoodPreferenceID, Name) VALUES (5, 'Steakhouse cuisine');
+  INSERT INTO FoodPreferences(FoodPreferenceID, Name) VALUES (6, 'Seafood cuisine');
+  INSERT INTO FoodPreferences(FoodPreferenceID, Name) VALUES (7, 'Other');
